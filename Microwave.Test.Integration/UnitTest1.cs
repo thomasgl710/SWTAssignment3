d592a0364f4868b03126b592cd255182405b4b52
thomasgl710 <thomasgl10@gmail.com>
2020-11-19 13:48:46 +0100

    nice

--- Microwave.Test.Integration/UnitTest1.cs
+++ Microwave.Test.Integration/UnitTest1.cs
@@ -14,5 +14,10 @@ namespace Microwave.Test.Integration
         {
             Assert.Pass();
         }
+        [Test]
+        public   void Test2()
+        {
+            Assert.Pass();
+        }
     }
 }
